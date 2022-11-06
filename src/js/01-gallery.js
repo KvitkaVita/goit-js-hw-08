dbf199e6a0f331b328e8e5fe604ed1d7e46938ff
Viktoriia Semeniuk <zakharuk.vita@gmail.com>
2022-11-06 23:17:30 +0200

    adds 1 and 3(part)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+
+    const galleryMarkup = galleryItems.map(({original, preview, description}) => 
+    `
+        <div class ="gallery__item">
+                <a class="gallery__link" href="${original}">
+               <img class="gallery__image" 
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+                </a>
+                </div>
+        `).join("");
+    
+        gallery.insertAdjacentHTML("beforeend", galleryMarkup)
+console.log(galleryMarkup)
+
+let instance = "";
+ gallery.addEventListener("click", onImageClick)
+function onImageClick(event) {
+    event.preventDefault();
+if (event.target.nodeName !== "IMG") { 
+    return };
+instance = SimpleLightbox.create(
+     `<img src=${event.target.dataset.source}>`
+    );
+instance.show();
+document.addEventListener('keydown', onModalCloseToEscape); 
+    }
+    function onModalCloseToEscape (evt) {
+        if (evt.code === "Escape") {
+            instance.close();
+            document.removeEventListener("keydown", onModalCloseToEscape)
+} 
+    }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from "lodash.throttle"
 
+const STORAGE_KEY = "feedback-form-state";
+
+const refs = {
+   form: document.querySelector(".feedback-form"),
+   email: document.querySelector("input"),
+   textarea: document.querySelector("textarea"),
+};
+
+refs.form.addEventListener("submit", onFormSubmit)
+// refs.email.addEventListener("input", onEmailInput)
+refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
+
+populateMessage();
+// "feedback-form-state" - ключ до сховища
+
+function onFormSubmit(evt) {
+evt.preventDefault();
+
+evt.currentTarget.reset();
+
+localStorage.removeItem(STORAGE_KEY);
+}
+
+// нижче треба добавити тротл
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, message)
+}
+
+function populateMessage() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if(savedMessage) {
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage;
+    }
+}
