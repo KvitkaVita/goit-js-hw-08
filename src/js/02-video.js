44502e8a40e5b191b0df5ae00e1eaab101bc20c4
Viktoriia Semeniuk <zakharuk.vita@gmail.com>
2022-11-11 22:30:44 +0200

    adds task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,34 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
+
+player.setCurrentTime(
+  Number(localStorage.getItem('video=player-current-time)'))
+);
